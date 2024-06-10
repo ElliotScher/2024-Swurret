ea085318754b06ccf18a455b6d68fc6ad0b592ac
Elliot Scher <ecscher84@gmail.com>
2024-06-10 00:07:25 -0400

    Turret and Hood rotate properly, but hood doesn't fit the bearing hole.

--- src/main/java/frc/robot/util/Mechanism3d.java
+++ src/main/java/frc/robot/util/Mechanism3d.java
@@ -9,12 +9,12 @@ public class Mechanism3d {
 
   public static final Pose3d[] getPoses(Rotation2d turretAngle, Rotation2d hoodAngle) {
     Pose3d turretPose =
-        new Pose3d(0.0, 0.089667, 0.0, new Rotation3d(0.0, 0.0, turretAngle.getRadians()));
+        new Pose3d(0.0, 0.0, 0.1075 + 0.085, new Rotation3d(0.0, 0.0, turretAngle.getRadians()));
     Pose3d hoodPose =
         new Pose3d(
             0.0,
-            0.089667,
-            0.2515 + 0.085,
+            0.089,
+            0.25125 + 0.085,
             new Rotation3d(hoodAngle.getRadians(), 0.0, turretAngle.getRadians()));
 
     return new Pose3d[] {turretPose, hoodPose};
